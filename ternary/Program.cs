643eccdbc0bd868769d7eac61849f6674d58ee63
mikechittenden1974 <47178657+mikechittenden1974@users.noreply.github.com>
2019-04-15 14:22:25 +0100

    Update Program.cs

--- ternary/Program.cs
+++ ternary/Program.cs
@@ -6,12 +6,7 @@ namespace ternary
     {
         static void Main(string[] args)
         {
-            int x;
-            x = 10;
-            string result;
 
-            result = (x % 2 == 0) ? "Even Number" : "Odd number";
-            Console.WriteLine("{0} is {1}", x, result);
         }
     }
 }
