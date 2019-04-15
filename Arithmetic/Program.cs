2f8f5231dc680cb574cd09e4dfd608b5ae933ee6
mikechittenden1974 <47178657+mikechittenden1974@users.noreply.github.com>
2019-04-15 14:19:53 +0100

    Update Program.cs

--- Arithmetic/Program.cs
+++ Arithmetic/Program.cs
@@ -6,24 +6,7 @@ namespace Arithmetic
     {
         static void Main(string[] args)
         {
-            double x, y;
-                x = 160;
-                y = 50;
-
-            //Addition
-            Console.WriteLine("Addition : {0} + {1} = {2}", x, y, x + y);
-
-            //Subtraction
-            Console.WriteLine("Subtraction : {0} - {1} = {2}", x, y, x - y);
-
-            //Multiplication
-            Console.WriteLine("Multiplication : {0} * {1} = {2}", x, y, x * y);
-
-            //Division
-            Console.WriteLine("Division : {0} / {1} = {2}", x, y, x / y);
-
-            //Modulo
-            Console.WriteLine("Modulo : {0} % {1} = {2}", x, y, x % y);
+            
         }
     }
 }
