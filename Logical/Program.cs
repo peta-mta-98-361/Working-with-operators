9640a3ff34ed5add366df73ca3ca5f4e68ac021e
mikechittenden1974 <47178657+mikechittenden1974@users.noreply.github.com>
2019-04-15 14:20:34 +0100

    Update Program.cs

--- Logical/Program.cs
+++ Logical/Program.cs
@@ -6,20 +6,7 @@ namespace Logical
     {
         static void Main(string[] args)
         {
-            bool result;
-            int x, y;
-            x = 10;
-            y = 20;
-
-            //OR
-
-            result = (x == y) || (x > 5);
-            Console.WriteLine(result);
-
-            //AND
-
-            result = (x == y) && (x > 5);
-            Console.WriteLine(result);
+            
         }
     }
 }
