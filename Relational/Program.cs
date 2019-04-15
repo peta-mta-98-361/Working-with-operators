e3a2b3e500b40ed3269166420aea41f1128e5076
mikechittenden1974 <47178657+mikechittenden1974@users.noreply.github.com>
2019-04-15 14:20:53 +0100

    Update Program.cs

--- Relational/Program.cs
+++ Relational/Program.cs
@@ -6,28 +6,7 @@ namespace Relational
     {
         static void Main(string[] args)
         {
-            bool result;
-            int x, y;
-            x = 40;
-            y = 90;
 
-            result = (x == y);
-            Console.WriteLine("{0} == {1} returns {2}", x, y, result);
-
-            result = (x > y);
-            Console.WriteLine("{0} > {1} returns {2}", x, y, result);
-
-            result = (x >= y);
-            Console.WriteLine("{0} >= {1} returns {2}", x, y, result);
-
-            result = (x < y);
-            Console.WriteLine("{0} < {1} returns {2}", x, y, result);
-
-            result = (x <= y);
-            Console.WriteLine("{0} <= {1} returns {2}", x, y, result);
-
-            result = (x != y);
-            Console.WriteLine("{0} != {1} returns {2}", x, y, result);
         }
     }
 }
