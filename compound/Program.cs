ff7473b02bc159ebf06a44576f41de00de037ee2
mikechittenden1974 <47178657+mikechittenden1974@users.noreply.github.com>
2019-04-15 14:21:36 +0100

    Update Program.cs

--- compound/Program.cs
+++ compound/Program.cs
@@ -6,33 +6,6 @@ namespace compound
     {
         static void Main(string[] args)
         {
-            int number;
-            number = 12;
-
-            //+= is equivalent to number  = number + 5
-            Console.WriteLine("Value of number = {0}", number);
-            Console.WriteLine("\tnumber += 5 is equal to number + 5  = {0}", number += 5);
-            Console.WriteLine("\tn is now {0}\n", number);
-
-            //-= is equivalent to number  = number - 5
-            Console.WriteLine("Value of number = {0}", number);
-            Console.WriteLine("\tnumber -= 5 is equal to number - 5 =  {0}", number -= 5);
-            Console.WriteLine("\tn is now {0}\n", number);
-
-            //*= is equivalent to number  = number * 5
-            Console.WriteLine("Value of number = {0}", number);
-            Console.WriteLine("\tnumber *= 5 is equal to number * 5 = {0}", number *= 5);
-            Console.WriteLine("\tn is now {0}\n", number);
-
-            //This is equivalent to number  = number / 5
-            Console.WriteLine("Value of number = {0}", number);
-            Console.WriteLine("\tnumber /= 5 is equal to number / 5 = {0}", number /= 5);
-            Console.WriteLine("\tn is now {0}\n", number);
-
-            //This is equavalent to number = number % 5
-            Console.WriteLine("Value of number = {0}", number);
-            Console.WriteLine("\tnumber %= 5 is equal to number % 5 = {0}", number %= 5);
-            Console.WriteLine("\tn is now {0}\n", number);
 
         }
     }
